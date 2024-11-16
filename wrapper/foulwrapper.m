6ec7743ab1c80233890625a3f93a2d2b0fc245d3
und3fined <me@und3fined.com>
2024-11-16 16:28:25 +0700

    chore: print out filename

--- wrapper/foulwrapper.m
+++ wrapper/foulwrapper.m
@@ -314,5 +314,6 @@ int main(int argc, char *argv[]) {
   [fileManager removeItemAtPath:workingDir error:nil];
 
   fprintf(stderr, "[dump] Done!\n");
+  fprintf(stdout, "%s\n", [archiveName UTF8String]);
   return 0;
 }
