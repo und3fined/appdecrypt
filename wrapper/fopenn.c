68353fc96422445d082b5c30086ffee0a29cb63f
und3fined <me@und3fined.com>
2024-11-17 16:21:31 +0700

    chore: c func

--- wrapper/fopenn.c
+++ wrapper/fopenn.c
@@ -7,81 +7,42 @@
  *
  * Visit https://frida.re to learn more about Frida.
  */
-
-#include "frida-core.h"
-
-#include <glib.h>
-#include <glib-object.h>
-#include <gio/gio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <spawn.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
-static void on_signal(int signo);
-static gboolean stop(gpointer user_data);
+extern char **environ;
 
-static GMainLoop *loop = NULL;
+void spawn_process(const char *path, char *const argv[], char *const envp[]) {
+    posix_spawnattr_t attr;
+    posix_spawnattr_init(&attr);
+    posix_spawnattr_setflags(&attr, POSIX_SPAWN_START_SUSPENDED);
 
-int main(int argc, char *argv[]) {
-  guint target_pid;
-  FridaDeviceManager *manager;
-  GError *error = NULL;
-  FridaDeviceList *devices;
-  gint num_devices, i;
-  FridaDevice *local_device;
-  // FridaSession *session;
+    pid_t pid;
+    int status = posix_spawn(&pid, path, NULL, &attr, argv, envp);
+    if (status == 0) {
+        printf("Spawned process with PID: %d\n", pid);
+        // Resume the process if needed
+        // kill(pid, SIGCONT);
+    } else {
+        fprintf(stderr, "posix_spawn failed: %s\n", strerror(status));
+    }
 
-  frida_init();
-
-  if (argc != 2 || (target_pid = atoi (argv[1])) == 0) {
-    fprintf(stderr, "Usage: %s <bundle_id> [-R]\n\nOptions:\n-R: Enable remote mode. Port default 1337.\n", argv[0]);
-    return 1;
-  }
-
-  loop = g_main_loop_new(NULL, TRUE);
-
-  signal(SIGINT, on_signal);
-  signal(SIGTERM, on_signal);
-
-  manager = frida_device_manager_new();
-
-  devices = frida_device_manager_enumerate_devices_sync(manager, NULL, NULL);
-
-  local_device = NULL;
-  num_devices = frida_device_list_size(devices);
-  for (i = 0; i != num_devices; i++) {
-    FridaDevice *device = frida_device_list_get(devices, i);
-
-    fprintf(stderr, "[*] Found device: \"%s\"\n", frida_device_get_name(device));
-
-    if (frida_device_get_dtype(device) == FRIDA_DEVICE_TYPE_LOCAL)
-      local_device = g_object_ref(device);
-
-    g_object_unref(device);
-  }
-  g_assert(local_device != NULL);
-
-  frida_unref(devices);
-  devices = NULL;
-
-  frida_device_spawn_sync(local_device, "", NULL, NULL, &error);
-  g_assert(error == NULL);
-
-  frida_unref(local_device);
-
-  frida_device_manager_close_sync(manager, NULL, NULL);
-  frida_unref(manager);
-  g_print("[*] Closed\n");
-
-  g_main_loop_unref(loop);
-
-  return 0;
+    posix_spawnattr_destroy(&attr);
 }
 
-static void on_signal(int signo) { g_idle_add(stop, NULL); }
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
+        return 1;
+    }
 
-static gboolean stop(gpointer user_data) {
-  g_main_loop_quit(loop);
+    char *const prog_argv[] = {argv[1], NULL};
+    spawn_process(argv[1], prog_argv, environ);
 
-  return FALSE;
+    return 0;
 }
